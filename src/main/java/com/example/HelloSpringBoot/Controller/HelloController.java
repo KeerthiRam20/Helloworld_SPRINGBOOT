cd8083206482dfbd45c3b4a7360f2392770bf1f2
KeerthiRam20 <keerthisaiguru2000@gmail.com>
2023-04-02 13:55:54 +0530

    first commit

--- src/main/java/com/example/HelloSpringBoot/Controller/HelloController.java
+++ src/main/java/com/example/HelloSpringBoot/Controller/HelloController.java
@@ -5,10 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
-//    @GetMapping(\)
-//    public String
-
+    
     @GetMapping(value = "hi")
     public String HelloWorld(){
         return "HELLO WORLD!!! hi keerthi " ;
